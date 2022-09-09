d98583f78d4f59c2009340c6f2716ee626958b0e
Musazili <musaziliabamu1@gmail.com>
2022-09-09 15:25:23 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,8 +9,8 @@ int main(void)
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
-putchar('\n');
 for (letter = 'A'; letter <= 'Z'; letter++)
 putchar(letter);
+putchar('\n');
 return (0);
 }
