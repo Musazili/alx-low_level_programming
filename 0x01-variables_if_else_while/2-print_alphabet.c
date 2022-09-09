ff0eec908349e20d2de3275eac9353f0a09ad698
Musazili <musaziliabamu1@gmail.com>
2022-09-09 14:55:14 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *Return: always 0
+*/
+int main(void)
+{
+char letter;
+for (letter 'a'; letter <= 'z'; letter++)
+puchar(letter);
+putchar('\n');
+return (0);
+}
