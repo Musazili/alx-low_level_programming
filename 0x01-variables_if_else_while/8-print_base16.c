d1e9e85ddbaed7315810005b90eafa1d6a1f3dcf
Musazili <musaziliabamu1@gmail.com>
2022-09-10 09:57:29 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+*/
+int main(void)
+{
+int i = '0';
+char letter = 0;
+for (i = '0'; i <= '9'; i++)
+puchar(i % 10 + '0');
+{
+for (lettr = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
