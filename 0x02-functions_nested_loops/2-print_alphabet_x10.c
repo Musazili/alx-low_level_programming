52ec9660088bad2b8e71d755aa4506a480c5a418
Musazili <musaziliabamu1@gmail.com>
2022-09-13 09:35:16 -0700

    print alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
 */
 void print_alphabet_x10(void)
 {
-int n = 0
+int n = 0;
 char letter;
 for (n = 0; n <= 9; n++)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter)
-_putchar('\n')
+_putchar(letter);
+_putchar('\n');
 }
 }
