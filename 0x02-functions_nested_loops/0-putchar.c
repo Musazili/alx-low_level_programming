232cb5fd0e13ed09305aac39e3ebc08149f62040
Musazili <musaziliabamu1@gmail.com>
2022-09-13 07:54:19 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - print _putchar followed by a new line
+ *
+ *Reurn: always 0
+*/
+int main(void)
+{
+char letter[] = ('_', 'p', 'u', 't', 'c', 'h', 'a', 'r')
+int i;
+{
+for (i = 0; i <= 8; i++)
+_putchar(letter[i])
+}
+_putchar('\n')
+return (0);
+}
