f68ffd98f19fc4ca8e0c76480f7fb71e8f8cd518
Musazili <musaziliabamu1@gmail.com>
2022-09-13 08:34:26 -0700

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,8 +8,8 @@ int main(void)
 {
 char letter;
 {
-for (letter = 'a'; letter <= 'z'; lettet++)
-_putchat(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 }
 _putchar('\n');
 return (0);
