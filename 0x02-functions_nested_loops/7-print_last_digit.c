8615c633ffcebe682d8c1a56025656664099fb39
Musazili <musaziliabamu1@gmail.com>
2022-09-13 14:31:18 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ * @n: integer to be computed
+ * Return:  value of the last digit
+*/
+int print_last_digit(int n)
+{
+char n, lastDigit;
+lastDigit = n % 10
+_putchar("last digit = %d", lastDigit);
+return (0);
+}
