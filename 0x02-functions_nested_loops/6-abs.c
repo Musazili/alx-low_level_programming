5a9ce6a20c5b7fd030c45ca27279c4607b49e5fd
Musazili <musaziliabamu1@gmail.com>
2022-09-13 13:58:19 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - value of an integer
+ * @r: integer to be computed
+ *Return: the value of integer
+*/
+int _abs(int r)
+{
+if (r > 0)
+return (r);
+else
+{
+return (n * -1);
+}
