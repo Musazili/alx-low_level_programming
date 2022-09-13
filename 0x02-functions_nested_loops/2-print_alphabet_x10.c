70a3e155d4b5e7cc16ff64aedd0dcad25a982513
Musazili <musaziliabamu1@gmail.com>
2022-09-13 09:32:47 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ *Return: void
+*/
+void print_alphabet_x10(void)
+{
+int n = 0
+char letter;
+for (n = 0; n <= 9; n++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter)
+_putchar('\n')
+}
+}
