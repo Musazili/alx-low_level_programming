bde619b82134de9876144d66a8926b62ec2c50a5
Musazili <musaziliabamu1@gmail.com>
2022-09-14 14:23:35 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - print a times table
+ *
+ * Return: always 0
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar((i * j) + '0');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
