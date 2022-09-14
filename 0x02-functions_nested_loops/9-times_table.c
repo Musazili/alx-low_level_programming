1aac595e687df091adeb4cb5b09feefe41b1d777
Musazili <musaziliabamu1@gmail.com>
2022-09-14 15:54:58 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,15 +6,30 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, d;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j <= 9; j++)
 		{
-			_putchar((i * j) + '0');
-			_putchar(',');
-			_putchar(' ');
+			r = (i * j);
+			if ((r / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((r % 10) + '0');
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
