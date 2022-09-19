70b7a3b20d34e1fdbd7f85cfa89790fba606a857
Musazili <musaziliabamu1@gmail.com>
2022-09-19 16:19:20 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h:"
+/**
+ * puts2 - entry point
+ * @str: string to be printed
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i]
+		{
+			_putchar(str[i]);
+			i++;
+		}
+		_putchar('\n');
+		}
