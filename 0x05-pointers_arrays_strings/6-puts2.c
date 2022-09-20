685986c31bc8ad5f797a5ca07f0fbca5b4a14313
Musazili <musaziliabamu1@gmail.com>
2022-09-20 14:18:38 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * puts2 - entry point
  * @str: string to be printed
- * Return: 0
+ * Return: nothing
  */
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i) != 0)
+	while (*(str + i) != '\0')
 		{
 			if (i % 2 == 0)
 				_putchar(*(str + i));
 			i++;
 		}
-		_putchar('\n');
+		_putchar(10);
 }
