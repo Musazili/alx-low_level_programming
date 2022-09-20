ca25daefb371a43d9d596d0346ead71ac0d0d447
Musazili <musaziliabamu1@gmail.com>
2022-09-20 13:53:25 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,12 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	while (str[i] != \0)
+	while (*(str + i) != \0)
 		{
 			if (i % 2 == 0)
-			_putchar(str[i]);
+				_putchar(*(str + i));
 			i++;
 		}
 		_putchar('\n');
