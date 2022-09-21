c8199f22df67e2bb1e8cd5ea57a80e7ad7e559cc
Musazili <musaziliabamu1@gmail.com>
2022-09-21 10:58:57 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcat - entry point
+ * @src: string
+ * @dest: string to be concastinate
+ * Return: destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; src[i] != '\0'; i++, len++)
+	{
+		dest[len] = src[i];
+	}
+	dest[len] = '\0';
+	return (dest);
+}
