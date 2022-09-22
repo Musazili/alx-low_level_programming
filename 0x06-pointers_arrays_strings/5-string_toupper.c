d1b939070b679dd68974ed39e2812a0695879823
Musazili <musaziliabamu1@gmail.com>
2022-09-22 12:47:18 -0700

    string toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,7 +10,7 @@ char *string_toupper(char *n)
 
 	while   (*(n + i) != '\0';)
 	{
-		if ((*(n + i) >= 'a') && (*(n + i) >= 'z'))
+		if ((*(n + i) >= 97) && (*(n + i) >= 122))
 			*(n + i) = *(n + i) - 32;
 		i++;
 	}
