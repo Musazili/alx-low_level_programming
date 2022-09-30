d3433c98a0decbfb551dd674d18e5a38d0237625
Musazili <musaziliabamu1@gmail.com>
2022-09-30 12:41:54 -0700

    number of argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,10 +9,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	printf("%d", argc);
 	for (i = 0; i < argc - 1; i++)
 	{
-		printf("%s\n", *argv[i]);
+		printf("argv[%d]:%s\n", *argv[i]);
 	}
 	return (0);
 }
