436f81df6dd55f8c91e7b5340d0dc675354ed5e5
Musazili <musaziliabamu1@gmail.com>
2022-09-30 11:36:25 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: count
+ * @argv: number
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("what is my name: %s\n", argv[0]);
+}
