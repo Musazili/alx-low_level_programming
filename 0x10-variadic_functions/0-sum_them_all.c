240ad1eba143c64b437352e7c61cba9e2d6b13ae
Musazili <musaziliabamu1@gmail.com>
2022-10-15 13:11:47 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,8 +11,11 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 
 	va_start(nums, n);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(nums, int);
+
 	va_end(nums);
+
 	return (sum);
 }
